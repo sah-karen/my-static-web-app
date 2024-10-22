10e41130ecd97544dcbac64615bdbbe3cda65708
karen <sahkaren@gmail.com>
2024-10-22 20:59:53 +0400

    new product

--- vue-app/src/store/modules/products.js
+++ vue-app/src/store/modules/products.js
@@ -6,7 +6,7 @@ const data = {
   products: [
     {
       id: 10,
-      name: 'Strawberries',
+      name: 'New Strawberries',
       description: '16oz package of fresh organic strawberries',
       quantity: '1',
     },
